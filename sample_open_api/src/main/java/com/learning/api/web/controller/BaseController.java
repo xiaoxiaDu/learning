ee167497cc84a1bc8e2c4e432f0d847caa03400e
duxiaoxia <du.xiaoxia@ielpm.com>
2018-12-10 14:58:11 +0800

    rebase

--- sample_open_api/src/main/java/com/learning/api/web/controller/BaseController.java
+++ sample_open_api/src/main/java/com/learning/api/web/controller/BaseController.java
@@ -1,8 +1,11 @@
 package com.learning.api.web.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * 抽象基础控制类
  * Created by Administrator on 2018/12/10.
  */
+@Controller
 public abstract class BaseController {
 }
