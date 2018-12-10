a554ce126d1198c8ef5d4ec0f2f1ea60d2acddfd
duxiaoxia <du.xiaoxia@ielpm.com>
2018-12-10 15:02:30 +0800

    rebase

--- sample_open_api/src/main/java/com/learning/api/web/controller/BaseController.java
+++ sample_open_api/src/main/java/com/learning/api/web/controller/BaseController.java
@@ -1,6 +1,11 @@
 package com.learning.api.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 抽象基础控制类
@@ -8,7 +13,9 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public abstract class BaseController {
-  @RequestMapping("/hi")
+    private static final Logger logger = LoggerFactory.getLogger(BaseController.class);
+
+    @RequestMapping("/hi")
     public String helloWorld(HttpServletRequest request){
         logger.info("Hello World! test 123!");
         return "hi!";
